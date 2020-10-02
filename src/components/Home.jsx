a41645ef9a783230c4dbc22b905af504eff06676
agtrevino <39744373+agtrevino@users.noreply.github.com>
2020-10-02 18:40:41 -0500

    updates made to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Grid, Menu, Segment } from "semantic-ui-react";
-
 import CheckedIn from "./CheckedIn";
 import CheckInForm from "./CheckInForm";
 
@@ -8,53 +7,56 @@ class Home extends Component {
   render() {
     const { activeItemHome, handleItemClickHome } = this.props;
     return (
-      <Grid>
-        <Grid.Column width={4}>
-          <Menu fluid vertical tabular>
-            <Menu.Item
-              name="Quick Check In"
-              active={activeItemHome === "Quick Check In"}
-              onClick={handleItemClickHome}
-            />
-            <Menu.Item
-              name="Checked In"
-              active={activeItemHome === "Checked In"}
-              onClick={handleItemClickHome}
-            />
-          </Menu>
-        </Grid.Column>
 
-        <Grid.Column stretched width={12}>
-          <Segment>
-            {activeItemHome === "Checked In" && (
-              <CheckedIn
-                services={this.props.services}
-                servicesOnClick={this.props.servicesOnClick}
-                selectPetOnClick={this.props.selectPetOnClick}
-                pets={this.props.pets}
-                appointments={this.props.appointments}
-                checkIn={this.props.checkIn}
-                finishOnClick={this.props.finishOnClick}
+      // <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <Menu fluid vertical tabular>
+              <Menu.Item
+                name="Quick Check In"
+                active={activeItemHome === "Quick Check In"}
+                onClick={handleItemClickHome}
               />
-            )}
-            {activeItemHome === "Quick Check In" && (
-              <CheckInForm
-                services={this.props.services}
-                servicesOnClick={this.props.servicesOnClick}
-                selectPetOnClick={this.props.selectPetOnClick}
-                pets={this.props.pets}
-                appointments={this.props.appointments}
-                checkIn={this.props.checkIn}
-                handleResultSelect={this.props.handleResultSelect}
-                handleSearchChange={this.props.handleSearchChange}
-                isLoading={this.props.isLoading}
-                results={this.props.results}
-                value={this.props.value}
+              <Menu.Item
+                name="Checked In"
+                active={activeItemHome === "Checked In"}
+                onClick={handleItemClickHome}
               />
-            )}
-          </Segment>
-        </Grid.Column>
-      </Grid>
+            </Menu>
+          </div>
+          <div className="col-8">
+            <Segment>
+              {activeItemHome === "Checked In" && (
+                <CheckedIn
+                  services={this.props.services}
+                  servicesOnClick={this.props.servicesOnClick}
+                  selectPetOnClick={this.props.selectPetOnClick}
+                  pets={this.props.pets}
+                  appointments={this.props.appointments}
+                  checkIn={this.props.checkIn}
+                  finishOnClick={this.props.finishOnClick}
+                />
+              )}
+              {activeItemHome === "Quick Check In" && (
+                <CheckInForm
+                  services={this.props.services}
+                  servicesOnClick={this.props.servicesOnClick}
+                  selectPetOnClick={this.props.selectPetOnClick}
+                  pets={this.props.pets}
+                  appointments={this.props.appointments}
+                  checkIn={this.props.checkIn}
+                  handleResultSelect={this.props.handleResultSelect}
+                  handleSearchChange={this.props.handleSearchChange}
+                  isLoading={this.props.isLoading}
+                  results={this.props.results}
+                  value={this.props.value}
+                />
+              )}
+            </Segment>
+          </div>
+        </div>
+      // </div>
+
     );
   }
 }
