1d9c90275a4b59deeefe6255ef2c1091a1995d6c
agtrevino <aaron.g.trevino@gmail.com>
2020-10-15 13:10:07 -0500

    updates made to history

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -5,47 +5,83 @@ class History extends Component {
   render() {
     const { pets, services } = this.props;
     return (
-      <Card.Group>
-        {pets.length > 0 && services.length ? (
-          pets.map((pet) => (
-            <Card>
-              <Card.Content>
-                <Card.Header>{pet.name}</Card.Header>
-                {pet.appointments.map((appointment) => (
-                  <div>
-                    <Card.Meta>Date: {appointment.date}</Card.Meta>
-                    <Card.Meta>Service: {appointment.service_id}</Card.Meta>
-                    <div>
-                      {pet.services.map(
-                        (service) =>
-                          service.id === appointment.service_id && (
-                            <Card.Meta>{service.name} </Card.Meta>
-                          )
-                      )}
+      <div className="row my-4">
+        <div className="col-12">
+          <ul className="collapsible">
+            {pets.length > 0 && services.length ? (
+              pets.map((pet) => (
+                <li>
+                  <div className="collapsible-header">
+                    {pet.name}
+                    <i className="material-icons options-icon">more_vert</i>
+                  </div>
+                  <div className="collapsible-body">
+                    <div className="row">
+                      <div className="col-md-8">
+                        <div className="row">
+                          {pet.appointments.map((appointment) => (
+                            <div className="w-100">
+                              <h2>
+                                <span>Last Appointment</span>
+                                <span style={{ float: "right" }}>
+                                  {appointment.date}
+                                </span>
+                              </h2>
+                              <hr/>
+                              <h4 className="history-title">Service</h4>
+                              <div>
+                                {pet.services.map(
+                                  (service) =>
+                                    service.id === appointment.service_id && (
+                                      <Card.Meta>
+                                        <ul className="service-list">
+                                          <li>
+                                            <h3 className="ml-3 history-info">{service.name}</h3>
+                                          </li>
+                                        </ul>
+                                      </Card.Meta>
+                                    )
+                                )}
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      <div className="col-md-4 options-right">
+                        <a className="list-option w-100 text-left">
+                          Appointment History
+                          <i className="card-icon material-icons left">
+                            history
+                          </i>
+                        </a>
+                        <a className="list-option w-100 text-left">
+                          Some Other Option
+                          <i className="card-icon material-icons left">
+                            history
+                          </i>
+                        </a>
+
+                        <a className="list-option-delete w-100 text-left">
+                          Delete Pet
+                          <i className="card-icon material-icons left">
+                            remove_circle_outline
+                          </i>
+                        </a>
+                      </div>
                     </div>
                   </div>
-                ))}
-              </Card.Content>
-              <Card.Content extra>
-                <div className="ui two buttons">
-                  <Button basic color="green">
-                    Approve
-                  </Button>
-                  <Button basic color="red">
-                    Decline
-                  </Button>
-                </div>
-              </Card.Content>
-            </Card>
-          ))
-        ) : (
-          <Card>
-            <Card.Content>
-              <Card.Header>No Pets</Card.Header>
-            </Card.Content>
-          </Card>
-        )}
-      </Card.Group>
+                </li>
+              ))
+            ) : (
+              <Card>
+                <Card.Content>
+                  <Card.Header>No Pets</Card.Header>
+                </Card.Content>
+              </Card>
+            )}
+          </ul>
+        </div>
+      </div>
     );
   }
 }
